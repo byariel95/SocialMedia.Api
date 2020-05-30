b6a38b8c29ed2b8aabe12e945786b909c375878d
Ariel Monterroso <bmonterrosoalvarado@gmail.com>
2020-05-29 20:05:54 -0600

    first controller and first model

---
+++ SocialMedia.Api/Controllers/PostController.cs
@@ -0,0 +1,18 @@
+﻿
+
+namespace SocialMedia.Api.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using Infrastructure.Repositories;
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostController : ControllerBase
+    {
+        public IActionResult GetPosts ()
+        {
+            var posts = new PostRepository().GetPosts();
+            return Ok(posts);
+        }
+    }
+}
---
+++ SocialMedia.Infrastructure/Repositories/PostRepository.cs
@@ -0,0 +1,27 @@
+﻿
+
+
+namespace SocialMedia.Infrastructure.Repositories
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using SocialMedia.Core.Entities;
+
+    public class PostRepository
+    {
+        public IEnumerable<Post> GetPosts()
+        {
+            var posts = Enumerable.Range(1, 10).Select(x => new Post
+            {
+                PostId = x,
+                Description = $"Description No. {x}",
+                Date = DateTime.Now,
+                Image = $"http://miimage.com/{x}",
+                UserId = x*2
+            });
+
+            return posts;
+        }
+    }
+}
